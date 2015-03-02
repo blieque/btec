628679cdca8e5e40434e5e1a5a18284fa4ec62bc
Blieque Mariguan <himself@blieque.co.uk>
2015-03-02 23:50:50 +0000

    added 9.1 + 18.1, 9.1 work, index.php formatting
    - Added 9.1 assignment (just an include of the network types document)
      and the 18.1 assignment (empty)
    - Completed the network types section for 9.1
    - Changed some formatting in index.php, making sure lines to not exceed
      80 characters in length

--- index.php
+++ index.php
@@ -3,14 +3,12 @@
 # includes
 
 require __DIR__ . '/includes/functions.php';
-// require __DIR__ . '/includes/class.parsedown.php';
 require __DIR__ . '/includes/class.headings-handler.php';
 require __DIR__ . '/vendor/autoload.php';
 
-
 # variables
 
-$unit_names	= array (
+$unit_names = array (
 	1  => 'Communication and Employability Skills',
 	2  => 'Computer Systems',
 	3  => 'Information Systems',
@@ -35,22 +33,34 @@ $unit_names	= array (
 	42 => 'Spreadsheet Modelling'
 );
 
-$is_given	= isset($_GET['a']);									// has an assignment or doc name been given?
-$output		= '<!DOCTYPE html><html><head><title>';
+$is_given = isset($_GET['a']); // has an assignment or doc name been given?
+
+// detect http or https
+$protocol = empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
+// trim "/btec/" and anything after it from address
+$address  = explode('/btec/', $_SERVER['REQUEST_URI'])[0];
+// put it all together
+$address  = $protocol . $_SERVER['SERVER_NAME'] . $address . '/btec/';
+
+// final output variable
+$output = '<!DOCTYPE html><html><head><title>';
 
 # script
 
 if ($is_given) {
-	$title			= null;												// probably 'Spreadsheet Modelling - Assignment 3' or 'Readme' or suchlike
-	$markdown_file	= null;												// path to markdown file's directory
+
+	// probably 'Spreadsheet Modelling - Assignment 3' or 'Readme' or suchlike
+	$title = null;
+	// path to markdown file's directory
+	$markdown_file = null;
 	
 	# these make conditions easier later
 
 	if (isset($_GET['s'])) {
 		
-		$ext	= $_GET['s'] == 'ext';
-		$doc	= $_GET['s'] == 'doc';
-		$rol	= $_GET['s'] == 'rol';
+		$ext = $_GET['s'] == 'ext';
+		$doc = $_GET['s'] == 'doc';
+		$rol = $_GET['s'] == 'rol';
 
 		global $ext, $doc, $rol;
 
@@ -62,45 +72,63 @@ if ($is_given) {
 
 	}
 
-	$split		= explode('.', $_GET['a']);
-	$_GET['a']	= strcmp(substr($_GET['a'], 1, 1), '.') == 0 ? '0' . $_GET['a'] : $_GET['a'];
+	$split = explode('.', $_GET['a']);
+	$_GET['a'] = strcmp(substr($_GET['a'], 1, 1), '.')
+				 == 0 ? '0' . $_GET['a'] : $_GET['a'];
 
-	if ($ext) {															// extra assignment page (see docs for detail)
+	if ($ext) { // extra assignment page (see docs for detail)
 
-		$markdown_file	= 'markdown/ext/' . strtolower($_GET['a']) . '.md';
-		$word_split		= explode('-', $split[1]);
+		if (strlen($_GET['a']) > 0) {
 
-		$title = $unit_names[intval($split[0])] . ' &ndash; Assignment ' . $word_split[0] . ' &ndash; Excerpt';
+			$markdown_file = 'markdown/ext/' . strtolower($_GET['a']) . '.md';
+			$word_split = explode('-', $split[1]);
 
-	} else if ($doc) {													// documentation page
+			$title = $unit_names[intval($split[0])] .
+					 ' &ndash; Assignment ' .
+					 $word_split[0] .
+					 ' &ndash; Excerpt';
 
-		$word_split	= str_replace('-', ' ', $split[2]);
-		$word_split	= ucwords(strtolower($word_split));
+		} else {
+
+			header('Location: ' . $address_full . '#excerpts');
+			exit;
+
+		}
+
+	} else if ($doc) { // documentation page
+
+		$word_split = str_replace('-', ' ', $split[2]);
+		$word_split = ucwords(strtolower($word_split));
 
 		$markdown_file = 'doc/' . strtolower($_GET['a']) . '.md';
 		$title = 'Documentation &ndash; ' . $word_split;
 
-	} else if ($rol) {													// readme or license (tacky id, yes)
+	} else if ($rol) { // readme or license (tacky id, yes)
 
 		$markdown_file = '' . strtoupper($_GET['a']) . '.md';
 		$title = ucfirst(strtolower($split[0]));
 
-	} else {															// standard assignment page
+	} else { // standard assignment page
 
 		$markdown_file = 'markdown/' . $_GET['a'] . '.md';
-		$title = $unit_names[intval($split[0])] . ' &ndash; Assignment ' . $split[1];
+		$title = $unit_names[intval($split[0])] .
+				 ' &ndash; Assignment ' .
+				 $split[1];
 
 	}
 
-	$output .= $title;													// add title text to title element in head
+	$output .= $title; // add title text to title element in head
 
-} else {															// index page, when no markdown file name has been given
+} else { // index page, when no markdown file name has been given
 
 	$output .= 'IT BTEC Assignments';
 
 }
 
-$output .= '</title><meta name="viewport" content="width=device-width,initial-scale=1"><link rel="stylesheet" href="/btec/css/main.css"><link rel="stylesheet" href="/btec/css/mobile.css" media="max-device-width:700px"></head><body><section>';
+$output .= '</title><meta name="viewport" content="width=device-width,initial' .
+		   '-scale=1"><link rel="stylesheet" href="/btec/css/main.css"><link ' .
+		   'rel="stylesheet" href="/btec/css/mobile.css" media="max-device-wi' .
+		   'dth:700px"></head><body><section>';
 
 if ($is_given) {
 
@@ -108,19 +136,21 @@ if ($is_given) {
 
 	if (is_readable($markdown_file)) {
 
-		$output	   .= '<h1>' . $title . '</h1>';							// add same title as before, but in an h1 in the body
-		$markdown	= file_get_contents($markdown_file);					// load the file into a variable for wikkid modificashunz
+		// add same title as before, but in an h1 in the body
+		$output	 .= '<h1>' . $title . '</h1>';
+		// load the file into a variable for wikkid modificashunz
+		$markdown = file_get_contents($markdown_file);
 		global $markdown;
 
 		# process includes
 
-		$markdown	= process_specials($markdown_file, $markdown);
-		$markdown	= process_includes($markdown);
+		$markdown = process_specials($markdown_file, $markdown);
+		$markdown = process_includes($markdown);
 
 		# downscale headings by one level
 
-		$markdown	= "\n" . $markdown;										// makes everything easier
-		$markdown	= str_replace("\n#", "\n##", $markdown);
+		$markdown = "\n" . $markdown; // makes everything easier
+		$markdown = str_replace("\n#", "\n##", $markdown);
 
 		# find markdown heading lines
 
@@ -128,7 +158,7 @@ if ($is_given) {
 
 		# id headings and contents list
 
-		$replacements		= HeadingsHandler::replacement_array($heading_lines[0], $_GET['a']);
+		$replacements = HeadingsHandler::replacement_array($heading_lines[0], $_GET['a']);
 
 		/* 
 		 * Below, I had to use a for loop to replace the markdown headings with
@@ -144,28 +174,35 @@ if ($is_given) {
 
 		for ($i = 0; $i < count($heading_lines[0]); $i++) { 
 
-			$position		= strpos($markdown,$heading_lines[0][$i]);
+			$position = strpos($markdown,$heading_lines[0][$i]);
 			if ($position !== false) {
-				$markdown	= substr_replace($markdown,$replacements[$i],$position,strlen($heading_lines[0][$i]));
+				$markdown = substr_replace($markdown,
+										   $replacements[$i],
+										   $position,
+										   strlen($heading_lines[0][$i]));
 			}
 
 		}
 
-		$contents 			= HeadingsHandler::contents($heading_lines[0]);
+		$contents = HeadingsHandler::contents($heading_lines[0]);
 		if ($contents != 1) {
-			$output			= preg_replace('/(?<=<\/head><body>)(?=<section>)/', '<aside>' . $contents . '</aside>', $output);
+			$output = preg_replace('/(?<=<\/head><body>)(?=<section>)/',
+								   '<aside>' . $contents . '</aside>',
+								   $output);
 		}
 
 		# parse what markdown is left that I haven't mutilated
 
-		$ciconia	= new_gfm();
-		$output	   .= $ciconia->render($markdown);
+		$ciconia = new_gfm();
+		$output .= $ciconia->render($markdown);
 
 
-	} else {															// no markdown file of the name given found
+	} else { // no markdown file of the name given found
 
-		header('HTTP/1.0 404 Not Found');									// send http 404 code
-		$output .= '<h1>404</h1>The assignment was not found or is not readable. You can view a list at the <a class="ref" href="/btec/">index</a>.';
+		header('HTTP/1.0 404 Not Found'); // send http 404 code
+		$output .= '<h1>404</h1>The assignment was not found or is not readab' .
+				   'le. You can view a list at the <a class="ref" href="/btec' .
+				   '/">index</a>.';
 
 	}
 
@@ -184,17 +221,27 @@ if ($is_given) {
 	 *
 	 */
 	
-	$output .= '<h1>BTEC IT</h1>An assignment was not specified. All of the available assignments are listed below.<ul>';
+	$output .= '<h1>BTEC IT</h1>An assignment was not specified. All of the a' .
+			   'vailable assignments are listed below.<ul>';
 
 	foreach (glob('markdown/*.md') as $filename) {
 
-		$path_split	= explode('/', $filename);				// e.g., ['markdown', '06.1.md']
-		$doc_split	= explode('.', end($path_split));		// e.g., ['06', '1', 'md']
-		$doc_unit	= $unit_names[intval($doc_split[0])];	// e.g., 'Software Design and Development'
-		$doc_id		= strcmp(substr($doc_split[0], 0, 1), '0') == 0 ? substr($doc_split[0], 1, 1) : $doc_split[0];
-		$doc_id	   .= '.' . $doc_split[1];
+		// e.g., ['markdown', '06.1.md']
+		$path_split = explode('/', $filename);
+		// e.g., ['06', '1', 'md']
+		$doc_split = explode('.', end($path_split));
+		// e.g., 'Software Design and Development'
+		$doc_unit = $unit_names[intval($doc_split[0])];
+		$doc_id	 = strcmp(substr($doc_split[0], 0, 1), '0')
+				   == 0 ? substr($doc_split[0], 1, 1) : $doc_split[0];
+		$doc_id .= '.' . $doc_split[1];
 
-		$output .= '<li><a href="/btec/' . $doc_id . '">' . $doc_unit . ' &ndash; Assignment ' . $doc_split[1] . '</a></li>';
+		$output .= '<li><a href="/btec/' .
+				   $doc_id . '">' .
+				   $doc_unit .
+				   ' &ndash; Assignment ' .
+				   $doc_split[1] .
+				   '</a></li>';
 
 	}
 
@@ -202,51 +249,58 @@ if ($is_given) {
 
 	foreach (glob('markdown/ext/*.md') as $filename) {
 
-		$path_split		= explode('/', $filename);
+		$path_split = explode('/', $filename);
 		preg_match('/[0-9]+/', $path_split[2], $doc_split);
-		$doc_unit		= $doc_split[0];
-		$doc_unit		= $unit_names[intval($doc_unit)];
+		$doc_unit = $doc_split[0];
+		$doc_unit = $unit_names[intval($doc_unit)];
 
 		preg_match('/(?<=-).*?(?=.md)/', $path_split[2], $doc_name);
-		$doc_name		= ucwords(strtolower(str_replace('-', ' ', $doc_name[0])));
+		$doc_name = ucwords(strtolower(str_replace('-', ' ', $doc_name[0])));
 
-		$output .= '<li><a href="/btec/ext/' . substr($path_split[2], 0, -3) . '">' . $doc_unit . ' &ndash; ' . $doc_name . '</a></li>';
+		$output .= '<li><a href="/btec/ext/' .
+				   substr($path_split[2], 0, -3) .
+				   '">' .
+				   $doc_unit .
+				   ' &ndash; ' .
+				   $doc_name .
+				   '</a></li>';
 
 	}
 
-	$output .= '</ul>You can also view the <a href="/btec/readme" class="ref">readme</a> and <a href="/btec/license" class="ref">license</a> documents, or the <a href="/btec/docs/index">documentation</a> for this website. Everything can also be viewed as source code on <a href="https://github.com/blieque/btec">GitHub</a>, the home of its public repository..';
+	$output .= '</ul>You can also view the <a href="/btec/readme" class="ref"' .
+			   '>readme</a> and <a href="/btec/license" class="ref">license</' .
+			   'a> documents, or the <a href="/btec/docs/index">documentation' .
+			   '</a> for this website. Everything can also be viewed as sourc' .
+			   'e code on <a href="https://github.com/blieque/btec">GitHub</a' .
+			   '>, the home of its public repository..';
 
 }
 
-$output		   .= '</section></body></html>';
+$output .= '</section></body></html>';
 
-$protocol		= empty($_SERVER['HTTPS']) ? 'http://' : 'https://';	// detect http or https
-$prefix			= explode('/btec/', $_SERVER['REQUEST_URI'])[0];		// trim "/btec/" and anything after it from address
-$prefix_full	= $protocol . $_SERVER['SERVER_NAME'] . $prefix . '/btec/';	// lets go adding that "/btec/" part back, eh?
-
-
-# change semi-absolute links into real ones, by adding protocol and domain before link addresses
+# change semi-absolute links into absolute ones, by adding protocol and domain
+# before link addresses
 
 $output = str_replace(
 
 	'"/btec/',
-	'"' . $prefix_full,
+	'"' . $address,
 	$output
 
 );
 
 # add ext class to external anchors
 
-$prefix_full	= preg_quote($prefix_full, "/");
+$prefix_full = preg_quote($address, "/");
 preg_match_all("/<a href=\"(?=$prefix_full)/", $output, $anchors);
-$anchors		= $anchors[0];
+$anchors = $anchors[0];
 
 foreach ($anchors as $anchor) {
 
 	if (strcmp(substr($anchor, 9, strlen($prefix_full)), $prefix_full) == 0) {
 
-		$anchor_new	= str_replace('href', 'class="ext" href', $anchor);
-		$output		= str_replace($anchor, $anchor_new, $output);
+		$anchor_new = str_replace('href', 'class="ext" href', $anchor);
+		$output = str_replace($anchor, $anchor_new, $output);
 
 	}
 
@@ -254,11 +308,13 @@ foreach ($anchors as $anchor) {
 
 # un-escape escaped characters (hello /r/shittyprogramming)
 
-$output	= str_replace('ESCAPED-ASTERISK', '*', $output);				// un-escape escaped asterisks, episode 2
+// un-escape escaped asterisks, episode 2
+$output = str_replace('ESCAPED-ASTERISK', '*', $output);
 
 # semi-minify output
 
-$output	= minify($output);												// function from functions.php
+// function from functions.php
+$output = minify($output);
 
 # the big reveal
 
