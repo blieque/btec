cd5d97a16b075e52c23c518d874d5da6905f91ce
Blieque Mariguan <himself@blieque.co.uk>
2015-06-14 23:25:08 +0100

    completed the real part of unit 20
    - Finished the 20.2 and 20.3 websites. Now to write about them.

---
+++ file/sites/20.3-cubes/scripts/class.user.js
@@ -0,0 +1,77 @@
+function User() {
+
+    /* properties */
+
+    this.nameFirst = null;
+    this.nameLast = null;
+    this.age = null;
+
+    /* methods */
+
+    this.nameFull = function() {
+
+        /* Returns full name generated from first and last name. */
+
+        var out;
+
+        if (this.nameFirst != null ||
+            this.nameLast != null) { // if one or more names have been entered
+
+            var nameFirst = this.nameFirst == null ? '' : this.nameFirst,
+                nameLast = this.nameLast == null ? '' : this.nameLast,
+                space = this.nameFirst != null &&
+                        this.nameLast != null ? ' ' : '';
+            out = nameFirst + space + nameLast;
+        } else {
+            out = '(none given)';
+        }
+
+        return out;
+
+    }
+
+    this.ageFormatted = function() {
+
+        /* Returns age as a string. */
+
+        var ageStr;
+
+        if (this.age == null) {
+            ageStr = '(none given)';
+        } else {
+            ageStr = this.age + ' years';
+        }
+
+        return ageStr;
+
+    }
+
+    this.clear = function() {
+
+        this.nameFirst = null;
+        this.nameLast = null;
+        this.age = null;
+
+    }
+
+    this.updateData = function(data) {
+
+        /* Updates user object from data object. */
+
+        this.nameFirst = data.nameFirst;
+        this.nameLast = data.nameLast;
+        this.age = data.age;
+
+    }
+
+    this.exportData = function() {
+
+        /* Exports unique user data for localStorage. */
+
+        return { nameFirst: this.nameFirst,
+                 nameLast: this.nameLast,
+                 age: this.age };
+
+    }
+
+}
--- file/sites/20.3-cubes/scripts/user-data.js
+++ file/sites/20.3-cubes/scripts/user-data.js
@@ -1,32 +1,82 @@
-function User(nameFirst, age) {
+/* functions */
 
-    // known data
-    this.nameFirst = nameFirst;
-    this.age = age;
+function getUserData(isInit) {
 
-    // empty data
-    this.nameLast = '';
+    // initialise temporary objects for raw and formatted data
+    var data = { nameFirst: null,
+                 nameLast: null,
+                 age: null },
+        echo = { nameFirst: '',
+                 nameLast: '',
+                 age: '' };
 
-    // full name generated from first and last name
-    this.nameFull = function() {
-        var space = this.nameFirst.length > 0 &&
-                    this.nameLast.length > 0 ? ' ' : '';
-        return this.nameFirst + space + this.nameLast;
+    if (!isInit) {
+        echo.nameFirst = localUser.nameFirst == null ? '' : localUser.nameFirst;
+        echo.nameLast = localUser.nameLast == null ? '' : localUser.nameLast;
+        echo.age = localUser.age == null ? '' : localUser.age;
     }
+
+    data.nameFirst = prompt('What\'s your first name?', echo.nameFirst);
+    
+    if (isInit) {
+        data.nameLast = null;
+    } else {
+        // don't ask for a last name when the page loads
+        data.nameLast = prompt('What\'s your last name?', echo.nameLast);
+    }
+
+    var ageInput = prompt('How old are you, in years?', echo.age),
+        ageInt = parseInt(ageInput);
+
+    while (ageInput != '' && // user doesn't leave field blank
+           ageInput != null && // user doesn't cancel prompt
+           (ageInt < 0 || isNaN(ageInt))) { // user submits invalid age
+
+        ageInput = prompt('Please enter a valid age or nothing.\n' +
+                     'How old are you, in years?', ageInput);
+        ageInt = parseInt(ageInput);
+
+    }
+
+    if (ageInput == '') { // user left field blank
+        data.age = null;
+    } else if (ageInput != null) { // ...but didn't cancel prompt
+        data.age = ageInt;
+    }
+
+    // prevent cancelled prompts from removing data
+    data.nameFirst = data.nameFirst == null ?
+                     localUser.nameFirst : data.nameFirst;
+    data.nameLast = data.nameLast == null ?
+                    localUser.nameLast : data.nameLast;
+    data.age = data.age == null ?
+               localUser.age : data.age;
+
+    return data;
 
 }
 
-// executed once everything has loaded
-window.onload = function() {
+/* event handlers */
 
-    // prompt for user info
-    var nameFirst = prompt('What\'s your first name?',''),
-        age = prompt('How old are you?','');
+window.onload = function() { // fired once everything has loaded
 
     // globals
+    localUser = new User();
+
+    var data;
+    if (typeof localStorage.localUserData === 'undefined') {
+        // prompt for user info
+        data = getUserData(true);
+    } else {
+        // read in data from localStorage
+        data = JSON.parse(localStorage.localUserData);
+    }
+
+    localUser.updateData(data);
+
     elemBtnView = document.getElementById('bv');
     elemBtnUpdate = document.getElementById('bu');
-    localUser = new User(nameFirst, age);
+    elemBtnClear = document.getElementById('bc');
     
     // "view user data" button event
     elemBtnView.onclick = function() {
@@ -37,20 +87,29 @@ window.onload = function() {
          * well, as the element will not otherwise be loaded. */
 
         alert('Full name: ' + localUser.nameFull() + ',\n' +
-              'Age: ' + localUser.age + '');
+              'Age: ' + localUser.ageFormatted() + '.');
 
-    }
+    };
 
     // "update data" button event
     elemBtnUpdate.onclick = function() {
+        data = getUserData(false);
+        localUser.updateData(data);
+    };
 
-        localUser.nameFirst = prompt('What\'s your first name?',
-                                     localUser.nameFirst);
-        localUser.nameLast  = prompt('What\'s your last name?',
-                                     localUser.nameLast);
-        localUser.age       = prompt('How old are you?',
-                                     localUser.age);
-
+    // "clear data" button event
+    elemBtnClear.onclick = function() {
+        localUser.clear();
+        localStorage.removeItem('localUserData');
     }
 
 }
+
+window.onunload = function() {
+    // save user data when the page is navigated away from, if it's worth it
+    if (localUser.nameFirst !== null &&
+        localUser.nameLast !== null &&
+        localUser.age !== null) {
+        localStorage.localUserData = JSON.stringify(localUser.exportData());
+    }
+};
